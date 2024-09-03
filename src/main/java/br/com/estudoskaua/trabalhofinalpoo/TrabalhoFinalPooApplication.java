62c77ee39e6d658d7ea7038a73380c70257e61cb
Kauã Rabelo <kauarabelo1@gmail.com>
2024-09-02 21:48:35 -0300

    Initial commit

---
+++ src/main/java/br/com/estudoskaua/trabalhofinalpoo/TrabalhoFinalPooApplication.java
@@ -0,0 +1,13 @@
+package br.com.estudoskaua.trabalhofinalpoo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrabalhoFinalPooApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrabalhoFinalPooApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/estudoskaua/trabalhofinalpoo/TrabalhoFinalPooApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.estudoskaua.trabalhofinalpoo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrabalhoFinalPooApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
